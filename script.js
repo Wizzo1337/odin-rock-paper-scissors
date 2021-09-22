5b064a95597dd9f61bd7de53d7ce727c25a0c3df
Thomas <thomaswilllson@yahoo.com>
2021-09-21 20:09:23 -0700

    finished function playround

--- script.js
+++ script.js
@@ -9,12 +9,30 @@ function computerPlay () {
     }
 };
 
-console.log(computerPlay());
 
 function playRound (playerSelection, computerSelection) {
-
+    let playerSelect = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelect === "Rock" && computerSelection === "Paper") {
+        return `You Lose! ${computerSelection} beats ${playerSelect}`
+    } else if (playerSelect === "Rock" && computerSelection === "Scissors") {
+        return `You Win! ${playerSelect} beats ${computerSelection}`
+    } else if (playerSelect === "Paper" && computerSelection === "Scissors") {
+        return `You Lose! ${computerSelection} beats ${playerSelect}`
+    } else if (playerSelect === "Paper" && computerSelection === "Rock") {
+        return `You Win! ${playerSelect} beats ${computerSelection}`
+    } else if (playerSelect === "Scissors" && computerSelection === "Paper") {
+        return `You win! ${playerSelect} beats ${computerSelection}`
+    } else if (playerSelect === "Scissors" && computerSelection === "Rock") {
+        return `You Lose! ${computerSelection} beats ${playerSelect}`
+    } else {
+        return `It's a tie!`
+    }
 }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+
+function game () {
+    playRound ()
+}
